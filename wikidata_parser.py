2708ca1a2cb43aedd5bb4833bca19c2efe9fd4c2
Thomas Dimson <tdimson@gmail.com>
2019-09-22 13:07:56 -0700

    Black formatting

--- wikidata_parser.py
+++ wikidata_parser.py
@@ -2,25 +2,26 @@ import ujson as json
 import time
 import pickle
 import sys
-from itertools import zip_longest 
+from itertools import zip_longest
 from collections import namedtuple
 
 
 GlobeCoordinate = namedtuple("GlobeCoordinate", ["latitude", "longitude", "altitude", "precision"])
 WikiDataEntry = namedtuple("WikiDataEntry", ["id", "sample_name", "sitelinks", "sample_coord"])
 
+
 def grouper(n, iterable, padvalue=None):
     "grouper(3, 'abcdefg', 'x') --> ('a','b','c'), ('d','e','f'), ('g','x','x')"
-    return zip_longest(*[iter(iterable)]*n, fillvalue=padvalue)
+    return zip_longest(*[iter(iterable)] * n, fillvalue=padvalue)
 
 
 class WikiDataParser:
     @classmethod
     def load(cls, path):
-        return pickle.load(open(path, 'rb'))
+        return pickle.load(open(path, "rb"))
 
     def dump(self, path):
-        pickle.dump(self, open(path, 'wb'), protocol=pickle.HIGHEST_PROTOCOL)
+        pickle.dump(self, open(path, "wb"), protocol=pickle.HIGHEST_PROTOCOL)
 
     @classmethod
     def parse_globe_coordinate(cls, claims, title="", line_id=""):
@@ -28,7 +29,7 @@ class WikiDataParser:
             return None
 
         coordinates = claims["P625"]
-        #if len(coordinates) != 1:
+        # if len(coordinates) != 1:
         #    print(json.dumps(coordinates, indent=2))
         #    raise RuntimeError(f"({title}) Found item with more than one co-ordinate")
 
@@ -41,10 +42,10 @@ class WikiDataParser:
         if "snaktype" not in mainsnak:
             print(mainsnak)
             raise RuntimeError(f"({line_id} {title}) Main snak snaktype")
-            
+
         if mainsnak["snaktype"] != "value":
             return None
-        
+
         if "datavalue" not in mainsnak:
             print(mainsnak)
             raise RuntimeError(f"({line_id} {title}) Main snak without data value")
@@ -52,29 +53,28 @@ class WikiDataParser:
         datavalue = mainsnak["datavalue"]
         if "type" not in datavalue or datavalue["type"] != "globecoordinate":
             print(datavalue)
-            raise RuntimeError(f"({line_id} {title}) Bad data type for globe coordinate") 
+            raise RuntimeError(f"({line_id} {title}) Bad data type for globe coordinate")
 
         value = datavalue["value"]
         return GlobeCoordinate(*(value[e] for e in ("latitude", "longitude", "altitude", "precision")))
-    
-    
+
     @classmethod
-    def parse_dump(cls, path, whitelisted_wikis=None): 
+    def parse_dump(cls, path, whitelisted_wikis=None):
         wiki_title_to_id = {}
         id_to_entry = {}
         start = time.time()
         all_json_time = 0
-        
-        whitelisted_wikis = whitelisted_wikis and set(whitelisted_wikis) 
-           
+
+        whitelisted_wikis = whitelisted_wikis and set(whitelisted_wikis)
+
         for i, line in enumerate(open(path)):
             if not line.startswith("{"):
                 continue
-               
+
             s_json = time.time()
             loaded = json.loads(line.rstrip(",\n"))
-            all_json_time += (time.time() - s_json)
-             
+            all_json_time += time.time() - s_json
+
             line_type = loaded["type"]
             line_id = loaded["id"]
 
@@ -85,7 +85,6 @@ class WikiDataParser:
                 print(json.dumps(loaded, indent=2))
                 print(line_type)
                 raise RuntimeError("Found non-item line")
-
 
             if "sitelinks" not in loaded:
                 print(loaded)
@@ -111,17 +110,17 @@ class WikiDataParser:
 
             sample_coord = cls.parse_globe_coordinate(loaded["claims"], sample_title, line_id)
 
-
             entry = WikiDataEntry(line_id, sample_title, sitelinks, sample_coord)
             id_to_entry[line_id] = entry
 
             if i % 10000 == 0:
                 end = time.time()
-                print(f"Reached {i} in {end - start}s [{100 * all_json_time / (end - start)}% in json] ({i / (end - start)} lines per second)")
+                print(
+                    f"Reached {i} in {end - start}s [{100 * all_json_time / (end - start)}% in json] ({i / (end - start)} lines per second)"
+                )
 
-            
         return cls(wiki_title_to_id, id_to_entry)
-    
+
     def __init__(self, wiki_title_to_id, id_to_entry):
         self.wiki_title_to_id = wiki_title_to_id
         self.id_to_entry = id_to_entry
--- wikipedia_parser.py
+++ wikipedia_parser.py
@@ -3,7 +3,7 @@ import json
 import pickle
 import xml.sax
 import wikitextparser as wtp
-import multiprocessing 
+import multiprocessing
 import sys
 import time
 import io
@@ -13,54 +13,53 @@ from collections import namedtuple, Counter, defaultdict, OrderedDict
 
 UnparsedRawPage = namedtuple("UnparsedRawPage", ["id", "title", "redirect", "text"])
 
+
 class WikiXMLHandler(xml.sax.ContentHandler):
     def __init__(self, queue, limit=None):
         super().__init__()
-        
+
         self.queue = queue
         self.element_count = 0
         self.page_count = 0
         self.in_page = False
         self.limit = limit
-        
+
         # Per page
         self.in_title = False
         self.title_buffer = None
         self.page_title = None
         self.page_text = None
         self.page_redirect = None
-        
+
         self.seen_page_revision = False
         self.in_revision = False
         self.in_revision_text = False
-        
+
         self.in_id = False
         self.id_buffer = None
-        
-        self.page_id = None 
+
+        self.page_id = None
         self.start_time = time.time()
 
-         
     def startElement(self, name, attrs):
         self.element_count += 1
-        
-            
+
         if self.in_title:
             raise RuntimeException(f"Encountered element {name} within a title")
         if self.in_revision_text:
             raise RuntimeException(f"Encountered element {name} within revision text")
-            
+
         if self.in_id:
             raise RuntimeException(f"Encountered element {name} within id")
-        
+
         if name == "page":
             if self.in_page:
                 raise RuntimeException("Recursive page")
 
             self.in_page = True
-            
+
         if self.in_page:
-            
+
             if name == "title":
                 if self.page_title:
                     raise RuntimeException("Encountered a second title for the page!")
@@ -74,7 +73,6 @@ class WikiXMLHandler(xml.sax.ContentHandler):
                 if attrs.getLength() != 1:
                     raise RuntimeException(f"More than one redirect attribute for {self.page_title}")
                 self.page_redirect = attrs.getValue("title")
-                
 
             elif name == "revision":
                 if self.seen_page_revision:
@@ -91,13 +89,13 @@ class WikiXMLHandler(xml.sax.ContentHandler):
     def endElement(self, name):
         if name == "page":
             self.handle_page()
-            
+
             self.in_page = False
             self.page_title = None
             self.page_text = None
             self.seen_page_revision = False
             self.page_redirect = None
-            
+
         if self.in_page:
             if name == "title":
                 self.in_title = False
@@ -116,9 +114,7 @@ class WikiXMLHandler(xml.sax.ContentHandler):
                 self.in_id = False
                 self.page_id = self.id_buffer.getvalue().strip()
                 self.id_buffer = None
-            
 
-        
     def characters(self, data):
         if self.in_title:
             self.title_buffer.write(data)
@@ -126,16 +122,12 @@ class WikiXMLHandler(xml.sax.ContentHandler):
             self.revision_text_buffer.write(data)
         elif self.in_id:
             self.id_buffer.write(data)
-            
-            
+
     def handle_page(self):
         self.page_count += 1
-        
-        self.queue.put(
-            UnparsedRawPage(self.page_id, self.page_title, self.page_redirect, self.page_text)
-        )
-        
-    
+
+        self.queue.put(UnparsedRawPage(self.page_id, self.page_title, self.page_redirect, self.page_text))
+
         if self.limit and self.page_count >= self.limit:
             raise StopIteration("Stopping")
         elif self.page_count % 10000 == 0:
@@ -146,88 +138,88 @@ class WikiXMLHandler(xml.sax.ContentHandler):
 class ParsedRawPage(namedtuple("ParsedRawPage", ["id", "title", "redirect", "links"])):
     @classmethod
     def dump_pages(cls, pages, path):
-        with open(path, 'wb') as f:
+        with open(path, "wb") as f:
             for page in pages:
                 f.write(msgpack.packb(page, use_bin_type=True))
 
     @classmethod
     def read_pages(cls, path):
-        with open(path, 'rb') as f:
-            unpacker = msgpack.Unpacker(f, raw=False, use_list=False, max_map_len=1024**2)
+        with open(path, "rb") as f:
+            unpacker = msgpack.Unpacker(f, raw=False, use_list=False, max_map_len=1024 ** 2)
             for (id, title, redirect, links) in unpacker:
                 yield ParsedRawPage(id, title, redirect, Counter(links))
 
     @classmethod
     def parsed_wikipedia_pages(cls, filename, limit=None, concurrency=None):
-	concurrency = concurrency or multiprocessing.cpu_count() * 2
-	def unparsed2parsed_worker(reader_queue, writer_queue): 
-	    while True:
-		unparsed_page = reader_queue.get()
-		if unparsed_page is None:
-		    return
+        concurrency = concurrency or multiprocessing.cpu_count() * 2
 
-		parsed = wtp.parse(unparsed_page.text)    
-		page = cls(unparsed_page.id, unparsed_page.title, unparsed_page.redirect, Counter(
-		    e.title.strip() for e in parsed.wikilinks
-		))
-		writer_queue.put(page)
+        def unparsed2parsed_worker(reader_queue, writer_queue):
+            while True:
+                unparsed_page = reader_queue.get()
+                if unparsed_page is None:
+                    return
 
-	reader_queue = multiprocessing.Queue(concurrency * 10)
-	writer_queue = multiprocessing.Queue()
-	processes = []
-	try:
-	    for i in range(concurrency):
-		p = multiprocessing.Process(
-		    target=unparsed2parsed_worker, 
-		    args=(reader_queue, writer_queue), 
-		    daemon=True
-		)
-		p.start()
-		processes.append(p)
+                parsed = wtp.parse(unparsed_page.text)
+                page = cls(
+                    unparsed_page.id,
+                    unparsed_page.title,
+                    unparsed_page.redirect,
+                    Counter(e.title.strip() for e in parsed.wikilinks),
+                )
+                writer_queue.put(page)
 
-	    handler = WikiXMLHandler(reader_queue, limit=limit)
-	    try:
-		xml.sax.parse(filename, handler)
-	    except StopIteration:
-		pass
+        reader_queue = multiprocessing.Queue(concurrency * 10)
+        writer_queue = multiprocessing.Queue()
+        processes = []
+        try:
+            for i in range(concurrency):
+                p = multiprocessing.Process(
+                    target=unparsed2parsed_worker, args=(reader_queue, writer_queue), daemon=True
+                )
+                p.start()
+                processes.append(p)
 
-	    for p in processes:
-		reader_queue.put(None)
+            handler = WikiXMLHandler(reader_queue, limit=limit)
+            try:
+                xml.sax.parse(filename, handler)
+            except StopIteration:
+                pass
 
+            for p in processes:
+                reader_queue.put(None)
 
-	    pages = []
-	    while True:
-		try:
-		    pages.append(writer_queue.get(False))
-		except queue.Empty:
-		    if any(p.is_alive() for p in processes):
-			continue
-		    else:
-			break
+            pages = []
+            while True:
+                try:
+                    pages.append(writer_queue.get(False))
+                except queue.Empty:
+                    if any(p.is_alive() for p in processes):
+                        continue
+                    else:
+                        break
 
-	    return pages
-	except:
-	    print("Exception raised, terminating subprocesses")
-	    for p in processes:
-		p.terminate()
-	    raise
+            return pages
+        except:
+            print("Exception raised, terminating subprocesses")
+            for p in processes:
+                p.terminate()
+            raise
+
 
 class WikipediaPage(namedtuple("WikipediaPage", ["id", "title", "aliases", "links", "inlinks"])):
     @classmethod
     def dump_pages(cls, pages, path):
-        with open(path, 'wb') as f:
+        with open(path, "wb") as f:
             for (id, title, aliases, links, inlinks) in pages:
-                f.write(msgpack.packb((
-                    id, title, list(aliases), links, inlinks
-                ), use_bin_type=True))
+                f.write(msgpack.packb((id, title, list(aliases), links, inlinks), use_bin_type=True))
 
     @classmethod
     def read_pages(cls, path):
-        with open(path, 'rb') as f:
-            unpacker = msgpack.Unpacker(f, raw=False, use_list=False, max_map_len=1024**2)
+        with open(path, "rb") as f:
+            unpacker = msgpack.Unpacker(f, raw=False, use_list=False, max_map_len=1024 ** 2)
             for (id, title, aliases, links, inlinks) in unpacker:
                 yield WikipediaPage(id, title, set(aliases), Counter(links), Counter(inlinks))
-                
+
     @classmethod
     def resolve_parsed_pages(cls, parsed_pages):
         title_to_wikipedia_page = {}
@@ -238,13 +230,9 @@ class WikipediaPage(namedtuple("WikipediaPage", ["id", "title", "aliases", "link
                 redirects[p.title] = p.redirect
             else:
                 title_to_wikipedia_page[p.title] = WikipediaPage(
-                    id=p.id,
-                    title=p.title,
-                    aliases=set(),
-                    links=p.links.copy(),
-                    inlinks=Counter(),
+                    id=p.id, title=p.title, aliases=set(), links=p.links.copy(), inlinks=Counter()
                 )
-        
+
         # Making redirect chainer
         print("Creating aliases")
         circle_count = 0
@@ -259,9 +247,9 @@ class WikipediaPage(namedtuple("WikipediaPage", ["id", "title", "aliases", "link
                     redirects[title] = None
                     circle_count += 1
                     break
-                
+
                 seen_pages.add(resolution_pointer)
-                
+
                 if resolution_pointer in title_to_wikipedia_page:
                     redirects[title] = resolution_pointer
                     title_to_wikipedia_page[resolution_pointer].aliases.add(title)
@@ -274,11 +262,13 @@ class WikipediaPage(namedtuple("WikipediaPage", ["id", "title", "aliases", "link
                     redirects[title] = None
                     unresolvable_count += 1
                     break
-        
+
         t = circle_count + unresolvable_count + resolved_count
         assert t == len(redirects), "Not tautology with all redirects"
-        print(f"Resolved {resolved_count} ({resolved_count / t}) redirects with {circle_count} ({circle_count / t}) cycles and {unresolvable_count} ({unresolvable_count / t}) unresolvables")
-                   
+        print(
+            f"Resolved {resolved_count} ({resolved_count / t}) redirects with {circle_count} ({circle_count / t}) cycles and {unresolvable_count} ({unresolvable_count / t}) unresolvables"
+        )
+
         print("Resolving deepest links")
         bad_link_count = 0
         good_link_count = 0
@@ -289,10 +279,12 @@ class WikipediaPage(namedtuple("WikipediaPage", ["id", "title", "aliases", "link
             for raw_link, count in p.links.items():
                 resolved = False
                 # Wikipedia links occasionally upper case first letter
-                for link in (raw_link, raw_link.capitalize()):    
+                for link in (raw_link, raw_link.capitalize()):
                     if link in redirects and redirects[link] is not None:
                         resolved_link = redirects[link]
-                        assert resolved_link in title_to_wikipedia_page, f"Bad redirect was formed from '{link}' to '{resolved_link}'!"
+                        assert (
+                            resolved_link in title_to_wikipedia_page
+                        ), f"Bad redirect was formed from '{link}' to '{resolved_link}'!"
                         resolved_links[resolved_link] += count
                         title_to_wikipedia_page[resolved_link].inlinks[p.title] += count
                         resolved = True
@@ -302,7 +294,7 @@ class WikipediaPage(namedtuple("WikipediaPage", ["id", "title", "aliases", "link
                         title_to_wikipedia_page[link].inlinks[p.title] += count
                         resolved = True
                         break
-                        
+
                 if not resolved:
                     # print(f"WARN: '{p.title}' contains unresolved link '{link}'")
                     if raw_link.startswith("File:") or raw_link.startswith("Image:"):
@@ -311,21 +303,22 @@ class WikipediaPage(namedtuple("WikipediaPage", ["id", "title", "aliases", "link
                         bad_link_count += count
                         if bad_link_count % 100000 == 0:
                             print(f"Sample bad link: '{p.title}' contains unresolved link '{link}'")
-                    continue        
+                    continue
                 else:
                     good_link_count += 1
-             
+
             p.links.clear()
             p.links.update(resolved_links)
-            
+
         t = good_link_count + bad_link_count
-        print(f"Found {good_link_count} ({good_link_count / t}) good links and {bad_link_count} ({bad_link_count / t}) bad links and {file_count} ({file_count / t}) file links")
-                
+        print(
+            f"Found {good_link_count} ({good_link_count / t}) good links and {bad_link_count} ({bad_link_count / t}) bad links and {file_count} ({file_count / t}) file links"
+        )
 
         while True:
             try:
                 _, val = title_to_wikipedia_page.popitem()
             except KeyError:
                 break
-                
+
             yield val
